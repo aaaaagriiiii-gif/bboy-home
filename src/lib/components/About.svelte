3637419a3b25e3830d7a4dba79fcf1435a8b4db9
aaaaagriiiii-gif <aaaaagriiiii@gmail.com>
2026-01-22 22:11:20 +0900

    Add files via upload

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Header from './Header.svelte';
+    import Footer from './Footer.svelte';
+</script>
+
+<Header />
+<main class="container">
+    <h2>비보이 브라더스 소개</h2>
+    <div class="about">
+        <figure class="image">
+            <img src="/images/about.jpg" alt="about">
+        </figure>
+        <div class="content">
+            <p>브레이크 댄스의 시초는 미국이지만, 현재는 전 세계 200여개 나라에 브레이크 댄스 씬이 존재합니다. 이 중에서도 한국은 매년 세계 대회에서 우수한 성격을 거두며 비오잉 강국으로 자리매김을 하고 있습니다. 때문인지 한국의 보지옹두출전하 영상에서는 한국 비보이들이 차원이 다르다는 댓시글이 많이 달기기도 하는데요. 한국의 비보이는 맘녕언ㅁ게 존하혐,, 이들은 함께 뭉쳐 쿠를 이루고 있습니다.</p>
+            <p>크루의 구성원들은 해외의 대회에 직접 출전하는 건 물로,ㄴ 심사위윈을 ㅏㅇ록 ㅣㅇㅆ스빈다. 또한 이들을 국내에서 대도히를 주최하여 한국의 보비이 씬과 브레이키 ㅇ물화를 소게[이 ㅗ개학 ㅗ잇씁니다. 레르 불을 여러붇릉리 꼭 알아야 할 네팀의 쿠르를 소개하고 자합니다.]</p>
+        </div>
+    </div>
+
+</main>
+<Footer />
+
+<style lang="scss">
+    .about {
+        display: flex;
+        gap: 50px;
+        figure, .content { width: 50%; }
+        figure img { width: 100%; }
+    }
+
+    .container {
+        background: #fff;
+    }
+
+</style>
---
+++ src/lib/components/Contact.svelte
@@ -0,0 +1,42 @@
+<script>
+    import Header from './Header.svelte';
+    import Footer from './Footer.svelte';
+
+</script>
+
+
+<Header />
+<main class="container">
+    <h2>최고의 무대를 선사하겠습니다.</h2>
+    <div class="contact">
+        <figure class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.9973181836704!2d126.7911681583078!3d37.484389657020394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b62db2d99db03%3A0xca2ab305aecb5f8f!2z6rK96riw64-EIOu2gOyynOyLnCDshozsgqzrj5kgMjYtOA!5e0!3m2!1sko!2skr!4v1769085524246!5m2!1sko!2skr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </figure>
+        <address>
+            <p>경기도 부천시 원미구 소사동 26-8, 3층</p>
+            <p>(주)더큰세상</p>
+            <p>Email: <a href="mailto:agri@gmail.com">agri@gmail.com</a></p>
+            <p>Tel:
+                <a href="tel:031-0000-0000">031-0000-0000</a>
+                <a href="tel:010-0000-0000">010-0000-0000</a>
+            </p>
+            <p>Fax: 031-0000-0000</p>
+        </address>
+    </div>
+
+</main>
+<Footer />
+
+<style lang="scss">
+    .contact {
+        display: flex;
+        gap: 50px;
+        figure, address { width: 50%; }
+        address { font-style: normal;}
+        a { color: inherit}
+    }
+
+    .container {
+        background: #fff;
+    }
+</style>
